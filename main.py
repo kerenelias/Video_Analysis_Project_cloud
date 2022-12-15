03adfafde098fa1fb901024c8747742dd4d58ef1
keren <kerenelias90@gmail.com>
2022-12-15 13:49:38 +0200

    fix main file

--- main.py
+++ main.py
@@ -1,227 +1,6 @@
 # [import]
-import base64
-import json
 import os
-
-from google.cloud import pubsub_v1
-from google.cloud import storage
-from google.cloud import translate_v2 as translate
-from google.cloud import vision
-from google.cloud import speech
-from google.cloud import texttospeech
 from google.cloud import videointelligence
-
-vision_client = vision.ImageAnnotatorClient()
-translate_client = translate.Client()
-publisher = pubsub_v1.PublisherClient()
-storage_client = storage.Client()
-
-project_id = os.environ["video-analysis-project-370709"]
-# [import]
-
-# [START functions_ocr_detect]
-def detect_text(bucket, filename):
-    print("Looking for text in image {}".format(filename))
-
-    futures = []
-
-    image = vision.Image(
-        source=vision.ImageSource(gcs_image_uri=f"gs://{bucket}/{filename}")
-    )
-    text_detection_response = vision_client.text_detection(image=image)
-    annotations = text_detection_response.text_annotations
-    if len(annotations) > 0:
-        text = annotations[0].description
-    else:
-        text = ""
-    print("Extracted text {} from image ({} chars).".format(text, len(text)))
-
-    detect_language_response = translate_client.detect_language(text)
-    src_lang = detect_language_response["language"]
-    print("Detected language {} for text {}.".format(src_lang, text))
-
-    # Submit a message to the bus for each target language
-    to_langs = os.environ["TO_LANG"].split(",")
-    for target_lang in to_langs:
-        topic_name = os.environ["TRANSLATE_TOPIC"]
-        if src_lang == target_lang or src_lang == "und":
-            topic_name = os.environ["RESULT_TOPIC"]
-        message = {
-            "text": text,
-            "filename": filename,
-            "lang": target_lang,
-            "src_lang": src_lang,
-        }
-        message_data = json.dumps(message).encode("utf-8")
-        topic_path = publisher.topic_path(project_id, topic_name)
-        future = publisher.publish(topic_path, data=message_data)
-        futures.append(future)
-    for future in futures:
-        future.result()
-
-
-# [END functions_ocr_detect]
-
-
-# [START message_validatation_helper]
-def validate_message(message, param):
-    var = message.get(param)
-    if not var:
-        raise ValueError(
-            "{} is not provided. Make sure you have \
-                          property {} in the request".format(
-                param, param
-            )
-        )
-    return var
-
-
-# [END message_validatation_helper]
-
-
-# [START functions_ocr_process]
-def process_image(file, context):
-    """Cloud Function triggered by Cloud Storage when a file is changed.
-    Args:
-        file (dict): Metadata of the changed file, provided by the triggering
-                                 Cloud Storage event.
-        context (google.cloud.functions.Context): Metadata of triggering event.
-    Returns:
-        None; the output is written to stdout and Stackdriver Logging
-    """
-    bucket = validate_message(file, "bucket")
-    name = validate_message(file, "name")
-
-    detect_text(bucket, name)
-
-    print("File {} processed.".format(file["name"]))
-
-
-# [END functions_ocr_process]
-
-
-# [START functions_ocr_translate]
-def translate_text(event, context):
-    if event.get("data"):
-        message_data = base64.b64decode(event["data"]).decode("utf-8")
-        message = json.loads(message_data)
-    else:
-        raise ValueError("Data sector is missing in the Pub/Sub message.")
-
-    text = validate_message(message, "text")
-    filename = validate_message(message, "filename")
-    target_lang = validate_message(message, "lang")
-    src_lang = validate_message(message, "src_lang")
-
-    print("Translating text into {}.".format(target_lang))
-    translated_text = translate_client.translate(
-        text, target_language=target_lang, source_language=src_lang
-    )
-    topic_name = os.environ["RESULT_TOPIC"]
-    message = {
-        "text": translated_text["translatedText"],
-        "filename": filename,
-        "lang": target_lang,
-    }
-    message_data = json.dumps(message).encode("utf-8")
-    topic_path = publisher.topic_path(project_id, topic_name)
-    future = publisher.publish(topic_path, data=message_data)
-    future.result()
-
-
-# [END functions_ocr_translate]
-
-
-# [START functions_ocr_save]
-def save_result(event, context):
-    if event.get("data"):
-        message_data = base64.b64decode(event["data"]).decode("utf-8")
-        message = json.loads(message_data)
-    else:
-        raise ValueError("Data sector is missing in the Pub/Sub message.")
-
-    text = validate_message(message, "text")
-    filename = validate_message(message, "filename")
-    lang = validate_message(message, "lang")
-
-    print("Received request to save file {}.".format(filename))
-
-    bucket_name = os.environ["result_videointelligence"]
-    result_filename = "{}_{}.txt".format(filename, lang)
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(result_filename)
-
-    print("Saving result to {} in bucket {}.".format(result_filename, bucket_name))
-
-    blob.upload_from_string(text)
-
-    print("File saved.")
-
-# [END functions_ocr_save]
-
-
-# [START speech_transcribe_multichannel]
-def transcribe_file_with_multichannel(speech_file):
-    """Transcribe the given audio file synchronously with
-    multi channel."""
-
-    client = speech.SpeechClient()
-
-    with open(speech_file, "rb") as audio_file:
-        content = audio_file.read()
-
-    audio = speech.RecognitionAudio(content=content)
-
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=44100,
-        language_code="en-US",
-        audio_channel_count=2,
-        enable_separate_recognition_per_channel=True,
-    )
-
-    response = client.recognize(config=config, audio=audio)
-
-    for i, result in enumerate(response.results):
-        alternative = result.alternatives[0]
-        print("-" * 20)
-        print("First alternative of result {}".format(i))
-        print(u"Transcript: {}".format(alternative.transcript))
-        print(u"Channel Tag: {}".format(result.channel_tag))
-# [END speech_transcribe_multichannel]
-
-# [START tts_synthesize_text_file]
-def synthesize_text_file(text_file):
-    """Synthesizes speech from the input file of text."""
-
-    client = texttospeech.TextToSpeechClient()
-
-    with open(text_file, "r") as f:
-        text = f.read()
-        input_text = texttospeech.SynthesisInput(text=text)
-
-    # Note: the voice can also be specified by name.
-    # Names of voices can be retrieved with client.list_voices().
-    voice = texttospeech.VoiceSelectionParams(
-        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
-    )
-
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3
-    )
-
-    response = client.synthesize_speech(
-        request={"input": input_text, "voice": voice, "audio_config": audio_config}
-    )
-
-    # The response's audio_content is binary.
-    with open("output.mp3", "wb") as out:
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
-
-
-# [END tts_synthesize_text_file]
-
 
 # # [START videointelligence_func]
 def videointelligence_func(event, context):
@@ -283,7 +62,6 @@ def videointelligence_func(event, context):
     result = operation.result(timeout=300)
 
     print("\n finnished processing.")
-
 # [END videointelligence_func]
 
 # [START functions_storage_trigger_func]
@@ -291,26 +69,12 @@ def storage_trigger_func(event, context):
     file = event['name']
     split_file = os.path.splitext(file)
     file_type = split_file[-1]
-    
     try:
-        # Check if the file is image 
-        if file_type in ['.png','.jpeg']:
-            process_image(event,context)
-
         # Check if the file is video
-        elif file_type == '.mp4':
+        if file_type == '.mp4':
             videointelligence_func(event,context)
-        
-        # Check if the file is audio 
-        elif file_type == '.mp3':
-            transcribe_file_with_multichannel(event)
-        
-        # Check if the file is text 
-        elif file_type == '.txt':
-            synthesize_text_file(event)
-
 
     except TypeError:
-        print("Only files are allowed")
+        print("Only video files are allowed")
 
 # [END functions_storage_trigger_func]
